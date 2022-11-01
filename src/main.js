f2ed4a8582f627dc7d9ee9f4ce38bd3a2ed48a4a
Iaroslav Seniuk <savuk7@icloud.com>
2022-11-01 16:05:49 +0200

    update_files

--- src/main.js
+++ src/main.js
@@ -2,10 +2,9 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./css/style.css";
 import { inputValue, liAdd } from "./refs";
 import { createLi, addNewLi } from "./markup";
-import { create } from "json-server";
 
 const LOCAL_STORAGE_KEY = "todo-list";
-// const dataItemsArr = getFromLocalStorage(LOCAL_STORAGE_KEY);
+let dataItemsArr = getFromLocalStorage(LOCAL_STORAGE_KEY);
 
 inputValue.addEventListener("submit", (event) => {
   event.preventDefault();
@@ -16,39 +15,56 @@ inputValue.addEventListener("submit", (event) => {
 
   const dataObj = createObjData(eventValue);
 
-  // saveToLocalStorage(dataObj);
+  saveToLocalStorage(dataObj);
 
   const markup = createLi(dataObj);
   addNewLi(liAdd, markup);
   console.log(eventValue);
+
+  event.target.reset();
 });
 
-// function saveToLocalStorage(value) {
-//   dataItemsArr.push(value);
+function saveToLocalStorage(value) {
+  dataItemsArr.push(value);
+  updateToLS(dataItemsArr);
+}
 
-//   localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(dataItemsArr));
-// }
+function updateToLS(arr) {
+  localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(arr));
+}
 
-// function createObjData(string) {
-//   return {
-//     value: string,
-//     checked: false,
-//     id: Date.now(),
-//   };
-// }
+function createObjData(string) {
+  return {
+    value: string,
+    checked: false,
+    id: Date.now(),
+  };
+}
 
-// function getFromLocalStorage(key) {
-//   const data = localStorage.getItem(key);
-//   return data ? JSON.parse(data) : [];
-// }
+function getFromLocalStorage(key) {
+  const data = localStorage.getItem(key);
+  return data ? JSON.parse(data) : [];
+}
 
-// function renderCurrentMarkup(array) {
-//   if (!array.length) {
-//     return;
-//   }
+function renderCurrentMarkup(array) {
+  if (!array.length) {
+    return;
+  }
 
-//   const currentMarkup = array.map(createLi).join("");
-//   addNewLi(liAdd, currentMarkup);
-// }
+  const currentMarkup = array.map(createLi).join("");
+  addNewLi(liAdd, currentMarkup);
+}
 
-// renderCurrentMarkup(dataItemsArr);
+renderCurrentMarkup(dataItemsArr);
+
+liAdd.addEventListener("click", onDellBtnClick);
+
+function onDellBtnClick(e) {
+  if (e.target.nodeName === "BUTTON") {
+    const btnId = e.target.closest(".item").dataset.id;
+    e.target.closest(".item").remove();
+    dataItemsArr = getFromLocalStorage(LOCAL_STORAGE_KEY);
+    dataItemsArr = dataItemsArr.filter(({ id }) => id !== +btnId);
+    updateToLS(dataItemsArr);
+  }
+}
