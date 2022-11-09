05e9a9070c7628b390115297041f7f56500983b7
Yuliia Cherkashyna <jul11091109@gmail.com>
2022-11-09 15:35:54 +0200

    add get method

--- src/api/index.js
+++ src/api/index.js
@@ -11,3 +11,12 @@ export function addData(data) {
     return response.json();
   });
 }
+
+export function getData() {
+  return fetch(BASE_URL).then((response) => {
+    if (!response.ok) {
+      throw new Error(response.statusText);
+    }
+    return response.json();
+  });
+}
--- src/main.js
+++ src/main.js
@@ -2,10 +2,9 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./css/style.css";
 import { inputValue, liAdd } from "./refs";
 import { createLi, addNewLi } from "./markup";
-import { addData } from "./api";
+import { addData, getData } from "./api";
 
 const LOCAL_STORAGE_KEY = "todo-list";
-let dataItemsArr = getFromLocalStorage(LOCAL_STORAGE_KEY);
 
 inputValue.addEventListener("submit", (event) => {
   event.preventDefault();
@@ -16,7 +15,6 @@ inputValue.addEventListener("submit", (event) => {
 
   const dataObj = createObjData(eventValue);
 
-  // saveToLocalStorage(dataObj);
   addData(dataObj).then((data) => {
     const markup = createLi(data);
     addNewLi(liAdd, markup);
@@ -27,15 +25,6 @@ inputValue.addEventListener("submit", (event) => {
   event.target.reset();
 });
 
-function saveToLocalStorage(value) {
-  dataItemsArr.push(value);
-  updateToLS(dataItemsArr);
-}
-
-function updateToLS(arr) {
-  localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(arr));
-}
-
 function createObjData(string) {
   return {
     value: string,
@@ -43,21 +32,14 @@ function createObjData(string) {
   };
 }
 
-function getFromLocalStorage(key) {
-  const data = localStorage.getItem(key);
-  return data ? JSON.parse(data) : [];
+function renderCurrentMarkup() {
+  getData().then((response) => {
+    const currentMarkup = response.map(createLi).join("");
+    addNewLi(liAdd, currentMarkup);
+  });
 }
 
-function renderCurrentMarkup(array) {
-  if (!array.length) {
-    return;
-  }
-
-  const currentMarkup = array.map(createLi).join("");
-  addNewLi(liAdd, currentMarkup);
-}
-
-renderCurrentMarkup(dataItemsArr);
+renderCurrentMarkup();
 
 liAdd.addEventListener("click", onDellBtnClick);
 
@@ -65,21 +47,11 @@ function onDellBtnClick(e) {
   if (e.target.nodeName === "BUTTON") {
     const btnId = e.target.closest(".item").dataset.id;
     e.target.closest(".item").remove();
-    dataItemsArr = getFromLocalStorage(LOCAL_STORAGE_KEY);
-    dataItemsArr = dataItemsArr.filter(({ id }) => id !== +btnId);
-    updateToLS(dataItemsArr);
   }
 }
 liAdd.addEventListener("click", handleChecked);
 function handleChecked(e) {
   if (e.target.nodeName === "P") {
     const id = e.target.closest(".item").dataset.id;
-    dataItemsArr.forEach((item) => {
-      if (item.id === Number(id)) {
-        item.checked = !item.checked;
-        e.target.closest(".item").classList.toggle("checked");
-        updateToLS(dataItemsArr);
-      }
-    });
   }
 }
