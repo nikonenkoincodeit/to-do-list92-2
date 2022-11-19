c12474cebfd48bb0dff3fa1dade2a94e268611c6
Mariya-Lutsenko <107764269+Mariya-Lutsenko@users.noreply.github.com>
2022-11-19 14:34:41 +0200

    add 1

--- src/main.js
+++ src/main.js
@@ -1,2 +1,27 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./css/style.css";
+
+import {formRef} from './refs';
+
+formRef.addEventListener('submit', getDataForm);
+ function getDataForm (event) {
+    event.preventDefault();
+    const message = event.target.message.value.trim();
+    
+    if (!message) {
+       return
+    }
+    console.log (message);
+    const data = creatObjectData (message);
+    console.log (data);
+
+
+
+
+ }
+
+ function creatObjectData (value) {
+return {value: value, id: Date.now(), checked: false}
+
+ }
+  
---
+++ src/refs/index.js
@@ -0,0 +1,1 @@
+export const formRef = document.querySelector('.form');
