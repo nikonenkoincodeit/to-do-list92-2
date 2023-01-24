95a8b8b67189493689f34e15f3049b8c99bc74b6
nikonenkoincodeit <75611895+nikonenkoincodeit@users.noreply.github.com>
2023-01-24 14:30:23 +0200

    start project

--- src/main.js
+++ src/main.js
@@ -1,27 +1,2 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./css/style.css";
-
-import { formRef } from "./refs";
-import { saveData } from "./api";
-
-formRef.addEventListener("submit", getDataForm);
-
-function getDataForm(event) {
-  event.preventDefault();
-
-  const message = event.target.message.value.trim();
-
-  if (!message) {
-    return;
-  }
-  console.log(message);
-  const data = creatObjectData(message);
-  console.log(data);
-
-  saveData(data);
-  event.target.reset();
-}
-
-function creatObjectData(value) {
-  return { value: value, id: Date.now(), checked: false };
-}
--- src/refs/index.js
+++
@@ -1,1 +0,0 @@
-export const formRef = document.querySelector('.form');
