45170bf75b0a3010be37b908f24db352c977d53b
UserBRAvo99 <108820828+UserBRAvo99@users.noreply.github.com>
2023-01-24 15:07:45 +0200

    commit

--- src/main.js
+++ src/main.js
@@ -1,2 +1,11 @@
+import { formEl } from "./refs";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./css/style.css";
+
+formEl.addEventListener("submit", onSubmit);
+function onSubmit(event) {
+  event.preventDefault();
+  const message = event.target.message.value.trim();
+  if (!message) return;
+  event.target.reset();
+}
