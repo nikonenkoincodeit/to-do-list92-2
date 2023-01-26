612c052bc04f5ce4026a249563718596fd48b950
Anatoliy Yakovenko <yakovenko.anatoliy@gmail.com>
2023-01-26 15:25:48 +0200

    complete toDo list

--- src/api/index.js
+++ src/api/index.js
@@ -3,7 +3,10 @@ const LOCALSTORAGE_KEY = "items";
 export function saveData(data) {
   const items = getData();
   items.push(data);
-  localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(items));
+  saveArrData(items);
+}
+export function saveArrData(item) {
+  localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(item));
 }
 export function getData() {
   try {
--- src/main.js
+++ src/main.js
@@ -1,6 +1,5 @@
 import { formEl, listEl } from "./refs";
-import { saveData } from "./api";
-import { getData } from "./api";
+import { saveData, saveArrData, getData } from "./api";
 import { createMarkup } from "./markup";
 
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -35,4 +34,33 @@ function init() {
 init();
 function addMarkup(markup) {
   listEl.insertAdjacentHTML("beforeend", markup);
+}
+listEl.addEventListener("click", removeTask);
+
+function removeTask(event) {
+  if (!event.target.classList.contains("button")) return;
+  const { parentEl, taskId } = getIdParent(event);
+  parentEl.remove();
+  const dataArr = getData();
+  const filteredArr = dataArr.filter((el) => {
+    return el.id !== +taskId;
+  });
+  saveArrData(filteredArr);
+}
+listEl.addEventListener("click", toggleTask);
+function toggleTask(event) {
+  if (!event.target.classList.contains("text")) return;
+  const { parentEl, taskId } = getIdParent(event);
+  parentEl.classList.toggle("checked");
+  const dataArr = getData();
+  const tasked = dataArr.find((el) => {
+    return el.id === +taskId;
+  });
+  tasked.checked = !tasked.checked;
+  saveArrData(dataArr);
+}
+function getIdParent(event) {
+  const parentEl = event.target.closest(".item");
+  const taskId = parentEl.dataset.id;
+  return { parentEl, taskId };
 }
