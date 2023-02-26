c0636cb487e27f5448d275ee1c28af86ef81cb96
Haidai Kostiantyn <thekos0596@gmail.com>
2023-02-26 15:24:08 +0200

    update

--- src/main.js
+++ src/main.js
@@ -6,15 +6,13 @@ import "./css/style.css";
 
 formEl.addEventListener("submit", onFormSubmit);
 listEl.addEventListener("click", deleteTask);
+listEl.addEventListener("click", onPClick);
 
 function deleteTask(evt) {
   if (evt.target.tagName !== "BUTTON") {
     return;
   }
-  const parent = evt.target.closest(".item");
-  // console.log(parent);
-  const taskId = parent.dataset.id;
-  // console.log(taskId);
+  const { parent, taskId } = getTaskId(evt);
   parent.remove();
   const tasks = onGetData().filter(({ id }) => {
     return id !== Number(taskId);
@@ -51,6 +49,27 @@ function init() {
   if (data.length === 0) return;
   const createdMarkup = createMarkup(data);
   addMarkup(createdMarkup);
+}
+
+function getTaskId(evt) {
+  const parent = evt.target.closest(".item");
+  const taskId = parent.dataset.id;
+  return { parent, taskId };
+}
+
+function onPClick(evt) {
+  if (evt.target.tagName !== "P") {
+    return;
+  }
+  const { parent, taskId } = getTaskId(evt);
+  parent.classList.toggle("checked");
+  const localArr = onGetData();
+  const obj = localArr.find(({ id }) => {
+    return id === Number(taskId);
+  });
+  console.log(obj);
+  obj.checked = !obj.checked;
+  saveData(localArr);
 }
 
 init();
