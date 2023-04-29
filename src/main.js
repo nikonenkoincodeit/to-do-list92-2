9f8ae0f3ea6bdbe9ffddf53cc3d332bf61d24259
SalOne22 <111443297+SalOne22@users.noreply.github.com>
2023-04-29 16:04:34 +0200

    completed to-do list

--- src/main.js
+++ src/main.js
@@ -1,23 +1,78 @@
-import { formEl } from "./refs";
-import { createArrayData } from "./api";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "./css/style.css";
+import { formEl, listEl } from './refs';
+import { createArrayData, getData, saveData } from './api';
+import { createMarkup } from './markup';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './css/style.css';
 
-formEl.addEventListener("submit", onSubmit);
+window.addEventListener('DOMContentLoaded', init);
+
+listEl.addEventListener('click', onListClickDeleteItem);
+listEl.addEventListener('click', onListClickUpdateCheck);
+
+function onListClickDeleteItem(evt) {
+  if (!evt.target.classList.contains('button')) return;
+
+  console.log('🚀 ~ file: main.js:13 ~ onListClick ~ id:', id);
+
+  const { parent, id } = getParentItem(evt.target);
+  parent.remove();
+
+  const items = getData();
+  const updated = items.filter((item) => Number(item.id) !== Number(id));
+  saveData(updated);
+}
+
+function onListClickUpdateCheck(evt) {
+  if (!evt.target.classList.contains('text')) return;
+
+  const { parent, id } = getParentItem(evt.target);
+
+  const isDone = parent.classList.toggle('checked');
+
+  const items = getData();
+  const item = items.find((item) => Number(item.id) === Number(id));
+  item.isDone = isDone;
+
+  saveData(items);
+}
+
+function getParentItem(node) {
+  const parent = node.closest('.item');
+  const id = parent.dataset.id;
+
+  return { parent, id };
+}
+
+formEl.addEventListener('submit', onSubmit);
 function onSubmit(event) {
   event.preventDefault();
   const value = event.target.message.value.trim();
   if (!value) {
     return;
   }
-  console.log("123");
+  console.log('123');
   //   const formData = Object.fromEntries(new FormData(event.target));
   //     console.log(formData);
   const items = createDataObj(value);
   createArrayData(items);
+  const markup = createMarkup([items]);
+  populateList(markup);
   event.target.reset();
 }
 
 function createDataObj(value) {
-  return { value, id: Date.now(), isDon: false };
+  return { value, id: Date.now(), isDone: false };
+}
+
+function init() {
+  const data = getData();
+
+  if (!data.length) return;
+
+  const markup = createMarkup(data);
+  populateList(markup);
+}
+
+function populateList(markup) {
+  listEl.innerHTML += markup;
 }
--- src/markup/index.js
+++ src/markup/index.js
@@ -1,9 +1,11 @@
 export function createMarkup(data = []) {
-  return data.map(({ id, checked, value }) => {
-    const classEl = checked ? "checked" : "";
-    return `<li class="item ${classEl}" data-id="${id}">
+  return data
+    .map(({ id, isDone, value }) => {
+      const classEl = isDone ? 'checked' : '';
+      return `<li class="item ${classEl}" data-id="${id}">
         <p class="text">${value}</p>
         <button type="button" class="button">x</button>
       </li>`;
-  });
+    })
+    .join('');
 }
